8097af272aa581ae9b27c26825d429bf2f8b800a
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-08-12 21:41:11 +0600

    Feature #2: writing code for sign up

---
+++ Project Code/includes/signup.inc.php
@@ -0,0 +1,15 @@
+<?php 
+
+if(isset($_POST['submit'])){
+    require"dbh.inc.php";
+
+    $fullName = $_POST['fullName'];
+    $userName = $_POST['uname'];
+    $userEmail = $_POST['uemail'];
+    $userPassword = $_POST['upass'];
+
+
+
+
+}
+
