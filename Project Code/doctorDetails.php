ea3ad2b8329b18a1aa3df1f3d2a26ff4a4cfbb7d
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-08-15 02:35:55 +0600

    Feature #1: Created demo page to show details about all the doctors

---
+++ Project Code/doctorDetails.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+     <!-- -css files- -->
+     <link rel="stylesheet" href="css/all.min.css" />
+     <link rel="stylesheet" href="css/bootstrap.min.css" />
+     <link rel="stylesheet" href="css/owl.carousel.css" />
+     <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+     <link rel="stylesheet" href="css/index.css" />
+     <link rel="stylesheet" href="css/index1Res.css" />
+     <link rel="stylesheet" href="css/doctorDetails.css">
+ 
+     <!-- js files -->
+ 
+     <script src="js/jquery.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/popper.min.js"></script>
+     <script src="js/owl.carousel.min.js"></script>
+     <script src="js/index.js"></script>
+</head>
+<body>
+    <section class="docBack">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- <img src="img/doc2.jpg" alt="Background"> -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="doctor">
+        <div class="container">
+            <div class="row py-5 my-5">
+                <div class="col-md-4">
+                    <div class="doc__img">
+                        <img src="img/author.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="docInfo">
+                        <h1>Dr. Abdur Rahim</h1>
+                        <h3>Speciality:</h3>
+                        <h4>Degree: </h4>
+
+                        <P><strong>Description:</strong> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro ipsam rerum totam nostrum dolor, quaerat obcaecati cupiditate! Quis, cupiditate deserunt nostrum, ipsam quasi voluptate eos itaque non harum commodi, molestias laboriosam obcaecati repellendus. Earum natus enim porro, aut nobis minima error, suscipit non quisquam aperiam odit voluptatibus maxime? Minima, debitis. Omnis aut earum neque veniam non eveniet laborum voluptatum, itaque nemo magnam distinctio totam quam dolores cum tempore odit fugit, harum iusto ipsa consectetur similique quis id. Architecto numquam quis, voluptates, et illum laudantium incidunt in, dignissimos laborum officiis illo! Perferendis temporibus quibusdam repellat iusto commodi. Dolorem aliquid aliquam atque id dicta adipisci maiores libero minus quisquam itaque est perspiciatis doloribus, quae odio dolores illum numquam tempora nulla quos possimus nobis! Necessitatibus temporibus ullam voluptates officia, optio eum. Dolore delectus veniam sunt accusamus omnis fuga obcaecati optio magnam beatae expedita laborum voluptates repellat, earum mollitia possimus nisi quas, nobis a, quo quos vero esse ratione debitis. Veniam culpa distinctio ipsum harum? Doloribus unde rem laborum ea neque? Doloremque sint earum mollitia quis tenetur fuga expedita veniam aliquid, accusamus distinctio dolore reprehenderit dolorem tempore quia magnam quo neque sed voluptates optio doloribus quod nam! Impedit exercitationem voluptatum minus cupiditate, soluta, alias corporis corrupti fugiat sint, hic adipisci! Autem commodi nulla, suscipit quae placeat, sunt nostrum iste quos assumenda quam unde eaque deserunt nobis at esse. Explicabo recusandae voluptatibus consequatur voluptate sequi maiores itaque quisquam, dolore, distinctio incidunt minus, cupiditate harum modi? Esse, iure sed porro quae, voluptatibus quis corrupti itaque accusantium sint quod modi consequatur enim labore. In eius impedit sint quod sequi nobis, adipisci exercitationem nemo dolorem rem ad consectetur maiores quae consequuntur nulla dolorum minima enim dicta aut amet dolor! Minima suscipit assumenda quibusdam sunt deleniti neque esse autem doloribus inventore corrupti. Itaque dolores est laboriosam, nostrum alias hic. </P>
+                        <a href="#" class="btn btn-info">Get Appointment</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
