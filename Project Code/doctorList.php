022fe67e7531e5482191a23a2d47d2ca45cdfae6
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-08-22 01:02:32 +0600

    Feature #13: edited the index file

--- Project Code/doctorList.php
+++ Project Code/doctorList.php
@@ -3,7 +3,7 @@ require 'includes/dbh.inc.php';
 
 
 $sql = "SELECT * FROM doctors";
-$result = mysqli_query($conn,$sql);
+$result = mysqli_query($conn, $sql);
 
 
 
@@ -13,6 +13,7 @@ $result = mysqli_query($conn,$sql);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,9 +25,10 @@ $result = mysqli_query($conn,$sql);
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/owl.carousel.css" />
     <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+
+    <link rel="stylesheet" href="css/doctorList.css">
     <link rel="stylesheet" href="css/index.css" />
     <link rel="stylesheet" href="css/index1Res.css" />
-    <link rel="stylesheet" href="css/doctorList.css">
 
     <!-- js files -->
 
@@ -36,41 +38,44 @@ $result = mysqli_query($conn,$sql);
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/index.js"></script>
 </head>
+
 <body>
     <header>
 
-    <?php
+        <?php
+
         include 'includes/menu.inc.php';
-    ?>
+        ?>
     </header>
     <section class="doctors">
         <div class="container">
             <div class="row">
-            <?php while($row = mysqli_fetch_assoc($result)) { ?>
+                <?php while ($row = mysqli_fetch_assoc($result)) { ?>
                 <div class="col-md-6">
-                   <div class="row">
-                   
-                       <div class="col-md-6">
+                    <div class="row">
+
+                        <div class="col-md-6">
                             <div class="doc__photo">
-                                    <a href="doctorDetails.php?page=doctorDetails&ID=<?php echo $row['doc_ID'] ?>"><img src="img/author.jpg" alt="<?php echo $row['doc_Fullname'] ?>"></a>
-                                </div>
-                                
-                       </div>
-                       <div class="col-md-6 mt-5">
+                                <a href="doctorDetails.php?page=doctorDetails&ID=<?php echo $row['doc_ID'] ?>"><img src="img/author.jpg" alt="<?php echo $row['doc_Fullname'] ?>"></a>
+                            </div>
+
+                        </div>
+                        <div class="col-md-6 mt-5">
                             <div class="doc__info">
-                                    <h3><?php echo $row['doc_Fullname'] ?></h3>
-                                    <h4><strong>Specialty:</strong> Consultant, <?php echo $row['doc_Specialization1'] ?> <?php echo $row['doc_Specialization2'] ?></h4>
-                                    <!-- <p class="docDegree"><strong>Degree:</strong> <?php echo $row['doc_Degree'] ?></p> -->
-                                    <p class="docTime"><strong>Time:</strong> <?php echo $row['doc_Time1'] ?>  <?php echo $row['doc_Time2'] ?> </p>
-                                    <a href="appointment.php?page=doctorAppointment&ID=<?php echo $row['doc_ID']?>"  class="btn btn-info">Make Appointment</a>
-                                    <a href="doctorDetails.php?page=doctorDetails&ID=<?php echo $row['doc_ID'] ?>" class="float-right btn btn-dark">View Details</a>
-                                </div>
-                       </div>
-                   </div>
+                                <h3><?php echo $row['doc_Fullname'] ?></h3>
+                                <h4><strong>Specialty:</strong> Consultant, <?php echo $row['doc_Specialization1'] ?> <?php echo $row['doc_Specialization2'] ?></h4>
+                                <!-- <p class="docDegree"><strong>Degree:</strong> <?php echo $row['doc_Degree'] ?></p> -->
+                                <p class="docTime"><strong>Time:</strong> <?php echo $row['doc_Time1'] ?> <?php echo $row['doc_Time2'] ?> </p>
+                                <a href="appointment.php?page=doctorAppointment&ID=<?php echo $row['doc_ID'] ?>" class="btn btn-info">Make Appointment</a>
+                                <a href="doctorDetails.php?page=doctorDetails&ID=<?php echo $row['doc_ID'] ?>" class="float-right btn btn-dark">View Details</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                   <?php } ?>
+                <?php } ?>
             </div>
         </div>
     </section>
 </body>
+
 </html>
--- Project Code/includes/menu.inc.php
+++ Project Code/includes/menu.inc.php
@@ -1,42 +1,58 @@
-<div class="menu-search">
-        <nav class="main-menu">
-          <div class="res-menu">
-            <!-- <a class="logo2" href="#">MediCare</a> -->
-            <a href="#"><i class="fas fa-bars"></i></a>
-          </div>
-          <ul class="text-center">
-            <li><a href="index.php">home</a></li>
-            <li><a href="doctorList.php">doctors</a></li>
-            <li>
-              <a href="javascript:void(0)">make appointment</a>
-              <ul>
-                <li><a href="#">sub menu</a></li>
-                <li><a href="#">sub menu</a></li>
-                <li><a href="#">sub menu</a></li>
-                <li><a href="#">sub menu</a></li>
-                <li><a href="#">sub menu</a></li>
-              </ul>
-            </li>
-            <li>
-              <a href="javascript:void(0)">search</a>
-              <!-- <ul>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Menu</title>
+
+
+</head>
+
+<body>
+  <div class="menu-search">
+    <nav class="main-menu">
+      <div class="res-menu">
+        <!-- <a class="logo2" href="#">MediCare</a> -->
+        <a href="#"><i class="fas fa-bars"></i></a>
+      </div>
+      <ul class="text-center">
+        <li><a href="index.php">home</a></li>
+        <li><a href="doctorList.php">doctors</a></li>
+        <li>
+          <a href="javascript:void(0)">make appointment</a>
+          <!-- <ul>
                 <li><a href="#">sub menu</a></li>
                 <li><a href="#">sub menu</a></li>
                 <li><a href="#">sub menu</a></li>
                 <li><a href="#">sub menu</a></li>
                 <li><a href="#">sub menu</a></li>
               </ul> -->
-            </li>
-            <li><a href="#">admin</a></li>
-            <li><a href="donor.php">Donor</a></li>
+        </li>
+        <li>
+          <a href="javascript:void(0)">search</a>
+          <!-- <ul>
+                <li><a href="#">sub menu</a></li>
+                <li><a href="#">sub menu</a></li>
+                <li><a href="#">sub menu</a></li>
+                <li><a href="#">sub menu</a></li>
+                <li><a href="#">sub menu</a></li>
+              </ul> -->
+        </li>
+        <li><a href="#">admin</a></li>
+        <li><a href="javascript:void(0)">Services</a>
+          <ul>
+            <li><a href="donor.php">Blood Donors</a>
             <li><a href="findAmbulance.php">Ambulance</a></li>
           </ul>
-        </nav>
-        <!-- <div class="search">
-              <input type="text" placeholder="Search Here.." />
-              <a href="#"
-                ><button type="button" name="button">
-                  <i class="fas fa-search"></i></button
-              ></a>
-            </div> -->
-      </div>
+
+        </li>
+
+      </ul>
+    </nav>
+
+  </div>
+</body>
+
+</html>
