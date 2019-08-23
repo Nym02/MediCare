69ec6101fbb7bafdbfdb7c945fcaf77e0b81ad6b
Sparrow48 <abu.nasib@northsouth.edu>
2019-08-24 00:31:59 +0600

    Feature #12 : add delete option for doctor list

--- Project Code/includes/adminMenu.inc.php
+++ Project Code/includes/adminMenu.inc.php
@@ -7,7 +7,7 @@
           <ul class="text-center">
             <li><a href="adminHome.php">Home</a> </li>
             <li><a href="updateDoctor.php">Update Doctor</a></li>
-            <li><a href="viewUser.php">View User</a></li>
+            <li><a href="deleteDoctor.php">Delete Doctor</a></li>
             <li><a href="update.php">Update User</a></li>
             <li><a href="delete.php">Delete User</a></li>
           </ul>
