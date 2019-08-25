19d8d613002f8c226ca340915d9acaaa9dd1fbd6
majhar-nayem <majharul_nayem@yahoo.com>
2019-08-25 22:12:38 +0600

    Facebook Api Integraded

--- Project Code/login.php
+++ Project Code/login.php
@@ -67,12 +67,105 @@
             <label id="pass__label1"></label>
           </div>
           
+          <?php
+        //	session_start();
+        if(!session_id()) {
+          session_start();
+      }
+					require_once __DIR__ . '/src/Facebook/autoload.php'; // download official fb sdk for php @ https://github.com/facebook/php-graph-sdk
+
+					$fb = new Facebook\Facebook([
+					  'app_id' => '519024735517232',
+					  'app_secret' => '2a5442e9adc3eeea6da36b01eb545da8',
+					  'default_graph_version' => 'v3.1',
+					  ]);
+
+					$helper = $fb->getRedirectLoginHelper();
+
+					$permissions = ['email']; // optional
+
+					try {
+						if (isset($_SESSION['facebook_access_token'])) {
+							$accessToken = $_SESSION['facebook_access_token'];
+						} else {
+					  		$accessToken = $helper->getAccessToken();
+						}
+					} catch(Facebook\Exceptions\FacebookResponseException $e) {
+					 	// When Graph returns an error
+					 	echo 'Graph returned an error: ' . $e->getMessage();
+
+					  	exit;
+					} catch(Facebook\Exceptions\FacebookSDKException $e) {
+					 	// When validation fails or other local issues
+						echo 'Facebook SDK returned an error: ' . $e->getMessage();
+					  	exit;
+					 }
+
+					if (isset($accessToken)) {
+						if (isset($_SESSION['facebook_access_token'])) {
+							$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+						} else {
+							// getting short-lived access token
+							$_SESSION['facebook_access_token'] = (string) $accessToken;
+
+						  	// OAuth 2.0 client handler
+              $oAuth2Client = $fb->getOAuth2Client();
+              
+              //echo $oAuth2Client;
+
+							// Exchanges a short-lived access token for a long-lived one
+							$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+
+							$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+
+							// setting default access token to be used in script
+							$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+						}
+
+						// redirect the user back to the same page if it has "code" GET variable
+						if (isset($_GET['code'])) {
+							header('Location: ./');
+						}
+
+						// getting basic info about user
+						try {
+							$profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
+              $profile = $profile_request->getGraphNode()->asArray();
+              $user = $profile_request->getGraphUser();
+              $_SESSION['name'] = $user->getField('name');
+						} catch(Facebook\Exceptions\FacebookResponseException $e) {
+							// When Graph returns an error
+							echo 'Graph returned an error: ' . $e->getMessage();
+							session_destroy();
+							// redirecting user back to app login page
+							header("Location: ./");
+							exit;
+						} catch(Facebook\Exceptions\FacebookSDKException $e) {
+							// When validation fails or other local issues
+							echo 'Facebook SDK returned an error: ' . $e->getMessage();
+							exit;
+						}
+
+						// printing $profile array on the screen which holds the basic info about user
+						print_r($user);
+
+					  	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
+					} else {
+      
+						// replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
+            $loginUrl = $helper->getLoginUrl('http://localhost/MediCare/Project%20Code/login.php', $permissions);
+            echo '<div class="loginWith">';
+            echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+            echo '</div>';
+
+
+
+					}
+
+?>
           <!-- <div class="loginWith">
-            <a href='#'>Log in with Facebook</a >
-          </div> -->
-          <div class="loginWith">
             <a href='#' scope="public_profile,email" onlogin="checkLoginState();">Log in with Facebook</a >
-          </div>
+          </div> -->
           <input
              class="btn btn-outline-dark "
              type="submit"
@@ -85,43 +178,12 @@
       </div>
       
       
-      <!-- <fb:login-button 
-  scope="public_profile,email"
-  onlogin="checkLoginState();">
-  </fb:login-button> -->
-      
       <div class="form-footer">
         <a href="signup.php">Create An Account.</a>
       </div>
     </div>
 
     <script src="js/form-validation.js"></script>
-    <script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '{519024735517232}',
-      cookie     : true,
-      xfbml      : true,
-      version    : '{api-version}'
-    });
-      
-    FB.AppEvents.logPageView();   
-      
-  };
-
-  function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
+  
   </body>
 </html>
